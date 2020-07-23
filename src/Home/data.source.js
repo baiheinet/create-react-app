ab512edc084d752d2f32a4fe9634f26b3f5f46fc
ke <baiheinet@gmail.com>
2020-07-23 18:04:35 +0800

    foot ico

--- src/Home/data.source.js
+++ src/Home/data.source.js
@@ -39,7 +39,7 @@ export const Footer20DataSource = {
       {
         name: 'image',
         children:
-          'https://gw.alipayobjects.com/zos/rmsportal/NuuAwJBxewWglRSoNjET.png',
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAABc0lEQVR4Xu2a263CQAxEvVQGlQCdQCWhE6iMRShsEpCCImGPHHH4udwfe/Bz7HWx16de7Gpm2/Z/0N9bOdjum2w1joIBiIDeAurQm0sDNQ5SgBrwXgOCiv8odmEXkOEYUiBcY1IFGCCpY2SwiIChC3R2kpl9qmhj1e7WO2L63QPMnOyN1bK381OFmgd4/CwPGcNMggHERMjDex4yiIBGyEgBUqDNAl34Nsgjd91klKPd3tqgm+SVCYIJrsxh7nCJAHEXYC2+cCMke59QEyEigAjgaSz326B7n/0UuDAFZDjgAeGmTq6ACEjuoHB4IxESrcXL8fv6vYpxwAQZhtpKjCMprsQ4k/vvO0HRWryto+cafBXjgAmGU63kCoiAgQiJcy9dDVCfqM4aQEzImAWYBZgFegtQA8TFJ18RXLqJ+fGY0W0j5IQDIpScqYbDIwLCTZxcwScTTA7XDx6Xoq9nevUs4OfC3yRxLE0KkAL9pcrf14AHxmQqUPKSVsQAAAAASUVORK5CYII=',
         className: 'copyright-logo',
       },
       {
